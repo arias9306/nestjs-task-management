d9c6b1d3b2e3c9e3b792aa488b5dc24333284db6
Andrés Arias <arias9306@gmail.com>
2020-07-28 21:19:09 -0500

    unit test

--- src/tasks/tasks.controller.spec.ts
+++ src/tasks/tasks.controller.spec.ts
@@ -1,18 +1,50 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { TasksController } from './tasks.controller';
+import { TasksService } from './tasks.service';
+import { Logger } from '@nestjs/common';
+import { Task, TaskStatus } from './task.model';
 
 describe('Tasks Controller', () => {
   let controller: TasksController;
+  let tasksService: TasksService;
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
       controllers: [TasksController],
+      providers: [TasksService, Logger],
     }).compile();
 
     controller = module.get<TasksController>(TasksController);
+    tasksService = module.get<TasksService>(TasksService);
   });
 
   it('should be defined', () => {
     expect(controller).toBeDefined();
+  });
+
+  it('should return an array of task', async () => {
+    const result: Task[] = [
+      {
+        description: 'description 1',
+        status: TaskStatus.OPEN,
+        id: '1',
+        title: 'task 1',
+      },
+    ];
+    jest.spyOn(tasksService, 'getAllTasks').mockImplementation(() => result);
+
+    expect(controller.getAllTasks()).toBe(result);
+  });
+
+  it('should return an new task', async () => {
+    const result: Task = {
+      description: 'description 1',
+      status: TaskStatus.OPEN,
+      title: 'new task',
+      id: '1',
+    };
+    jest.spyOn(tasksService, 'createTask').mockImplementation(() => result);
+
+    expect(controller.createTask('new task', 'description 1')).toBe(result);
   });
 });
--- src/tasks/tasks.service.spec.ts
+++ src/tasks/tasks.service.spec.ts
@@ -1,12 +1,14 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { TasksService } from './tasks.service';
+import { Logger } from '@nestjs/common';
 
 describe('TasksService', () => {
   let service: TasksService;
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
-      providers: [TasksService],
+      providers: [TasksService, Logger],
+
     }).compile();
 
     service = module.get<TasksService>(TasksService);
@@ -15,4 +17,23 @@ describe('TasksService', () => {
   it('should be defined', () => {
     expect(service).toBeDefined();
   });
+
+  it('should retrieve 3 task', () => {
+    service.createTask('task 1', 'description 1');
+    service.createTask('task 2', 'description 2');
+    service.createTask('task 3', 'description 3');
+
+    const tasks = service.getAllTasks();
+
+    expect(tasks.length).toBe(3)
+    expect(tasks[0].title).toBe('task 1');
+  })
+
+  it('should create a task', () => {
+    const newTask = service.createTask('task 1', 'description 1');
+
+    expect(newTask.title).toBe('task 1');
+    expect(newTask.description).toBe('description 1');
+    expect(newTask.id).not.toBe('')
+  })
 });
